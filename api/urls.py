bbd179e2a55ec741c7a5053f3776b60424b8beea
Toshinori Sumida <t.sumida1987@gmial.com>
2021-10-26 01:54:02 +0900

    migrate Task

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from api.views import TaskViewSet, CreateUserView, MyProfileView
+
+router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet, basename='tasks')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('register/', CreateUserView.as_view(), name='register'),
+    path('myself', MyProfileView.as_view(), name='myself')
+]
